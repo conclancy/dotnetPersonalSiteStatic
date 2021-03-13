682b8fe568772b6eeacf92b81a064e75a91ff4bf
connor <conclancy@hotmail.com>
2021-03-13 11:46:55 -0500

    Add Experience Component.
    Add experience razor component.

---
+++ dotnetPersonalSiteStatic/Data/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetPersonalSiteStatic.Data
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Logo { get; set; }
+        public List<Role> Roles { get; set; }
+    }
+}
---
+++ dotnetPersonalSiteStatic/Data/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetPersonalSiteStatic.Data
+{
+    public class Role
+    {
+        public string Title { get; set; }
+        public string StartDateString { get; set; }
+        public string EndDateString { get; set; }
+        public List<string> Experience { get; set; }
+    }
+}
